3e7b477bf780400eaef806d329719206cc85dcdc
Sebastian Vaughan <sebastian.vaughan@servicerocket.com>
2021-06-26 19:01:55 +1000

    feat: add eslint and prettier with config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        jest: true,
+        browser: true,
+        node: true,
+        es2021: true
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always']
+    }
+};
